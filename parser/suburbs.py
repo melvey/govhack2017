797fcc9dce86b106d506d5714cb82dd21f3ca238
ludwigdH <30543314+ludwigdH@users.noreply.github.com>
2017-07-29 20:02:05 +0930

    Add files via upload

--- parser/suburbs.py
+++ parser/suburbs.py
@@ -10,13 +10,19 @@ add_sub = ("INSERT INTO suburb "
                "(suburbID, suburbName) "
                "VALUES ({}, \"{}\")")
 
+subl=dict()
 with open(filename) as csvfile:
      reader = csv.DictReader(csvfile)
      for row in reader:
          sscCode = row['SSC_CODE_2016']
          sscName = row['SSC_NAME_2016']
-         print(sscCode, sscName)
-         cursor.execute(add_sub.format(sscCode, sscName))
-cnx.commit()
+         if sscCode not in subl.keys():
+             print(sscCode, sscName)
+             subl[sscCode]=sscName
+
+for a in subl:
+         print(add_sub.format(a, subl[a]))
+         cursor.execute(add_sub.format(a, subl[a]))
+         cnx.commit()
 
 cnx.close()
