9d3f70c75474f923b72e90573d622584d8bfbf9a
kingprovest <30543317+kingprovest@users.noreply.github.com>
2017-07-30 16:19:58 +0930

    Add files via upload

--- api/login.php
+++ api/login.php
@@ -1,20 +1,41 @@
 <?php
-require_once 'config.php';
+
 
 session_start();
+require_once 'config.php';
 
+$FullName=$_POST['fullname'];
 $Username=$_POST['username'];
 $Password =$_POST['password'];
-if isset($_SESSION['user']){
-	print "already logged in"
-}
-else{
-	if (!$row = mysqli_fetch_assoc($result))
+$Type =$_POST['type'];
+
+$StorePassword = password_hash($Password, PASSWORD_BCRYPT,array('cost'=>10));
+
+$sql ="INSERT INTO user(UserName,password,fullname,type) VALUES ('$Username',
+	  '$StorePassword','$FullName','$Type')";
+		echo $sql;
+		
+
+		$result = mysqli_query($conn, $sql);
+		if(!$row = mysqli_fetch_assoc($result))
 		{
-			echo ' Incorrect user name password';
+			echo ' Not inserting';
 			$_SESSION=array();
 			session_destroy();
-}
+			//header("refresh:3; url='accountbootstrap.php'");
+		}
+		else
+		{
+			
+			$_SESSION['UserID'] =$row['UserID'];
+			//var_dump ($_SESSION);		
+			//var_dump($_SESSION);
+
+		}
+	
+	
+
+
 
 
 
--- api/login_verify.php
+++ api/login_verify.php
@@ -1,52 +1,38 @@
 
  <?php
 
-		//session_id("Login");
+		
 		
 		session_start();
 		require_once 'config.php';
-		
+		//if(isset)
 		$Username=$_POST['username'];
 		$Password =$_POST['password'];
 		
-		$sql = "SELECT * FROM login WHERE UserName='$Username' ";
-		echo $sql;
+		
+		
+		$sql = "SELECT * FROM user WHERE UserName='$Username' AND Password='$Password'";
 		$result = mysqli_query($conn, $sql);
-		
-		if(password_verify($Password, $result['Password'])
-		{
-			session_start();
-			$_SESSION['UserID'] =$row['userID'];
-			
-		}
-		else
-		{
-			echo ' Incorrect user name password';
-			session_destroy();
-			
-		}
-		
-	
-	
-	/*if(!$row = mysqli_fetch_assoc($result))
+		if(!$row = mysqli_fetch_assoc($result))
 		{
 			echo ' Incorrect user name password';
 			$_SESSION=array();
 			session_destroy();
-			header("refresh:3; url='accountbootstrap.php'");
+			//header("refresh:3; url='accountbootstrap.php'");
 		}
 		else
-		{
+		{			
+			//$_SESSION['UserId'] = $row['UserID'];
+			//var_dump ($_SESSION);		
+			//header("refresh:2; url='index_postlogin.php'");
+			echo ' Login successfull';
 			
-			$_SESSION['id'] =$row['ID'];
-			var_dump ($_SESSION);		
-			header("refresh:2; url='index_postlogin.php'");
-			
-			var_dump($_SESSION);
+			//var_dump($_SESSION);
 
-		}*/
+		}
 		
-
+		
+		
 
 	
 ?>
